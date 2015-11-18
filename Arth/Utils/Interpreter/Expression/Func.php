8a44ddd4289efcee0838b37230d03366d8a0979a
Alexander Deider <for_inbox@mail.ru>
2015-11-19 01:31:09 +0600

    add some predefined functions

--- Arth/Utils/Interpreter/Expression/Func.php
+++ Arth/Utils/Interpreter/Expression/Func.php
@@ -37,3 +37,31 @@ class Func extends Expression
         return "{$this->name}($args)";
     }
 }
+
+Func::register('val', function($a) {
+    return $a;
+});
+Func::register('sin', function($a) {return sin($a); });
+Func::register('cos', function($a) {return cos($a); });
+Func::register('ln', function($a) {return log($a); });
+Func::register('lg', function($a) {return log($a, 10); });
+
+// aggregates
+Func::register('sum', function() {
+    return array_sum(func_get_args());
+});
+Func::register('min', function() {return min(func_get_args());});
+Func::register('max', function() {return min(func_get_args());});
+Func::register('avg', function() {
+    $mul = 1;
+    foreach (func_get_args() as $i => $n) {
+        $mul *= $n;
+    }
+   return pow($mul, 1/func_num_args());
+});
+Func::register('const', function($name) {
+    switch ($name) {
+        case 'pi': return M_PI;
+        case 'exp': return M_E;
+    }
+});
